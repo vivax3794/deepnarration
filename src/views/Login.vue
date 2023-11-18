0213a0c7c540480d7e623851bc7eb58c4a152552
vivax3794 <github@vivax3794.com>
2023-11-19 00:12:39 +0100

    use current host in login redirect

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <v-tooltip text="We require discord so we can ping you on discord when results are ready!" location="top">
     <template v-slot:activator="{ props }">
       <v-btn v-bind="props" id="login" color="blue" size="x-large" rounded="lg" prepend-icon="mdi-login"
-        href="https://discord.com/api/oauth2/authorize?client_id=1142575897753440288&redirect_uri=https%3A%2F%2Fprivate.vivax.dev%2F&response_type=token&scope=identify">
+        :href="`https://discord.com/api/oauth2/authorize?client_id=1142575897753440288&redirect_uri=https%3A%2F%2F${host}%2F&response_type=token&scope=identify`">
         Login with discord
       </v-btn>
     </template>
@@ -20,3 +20,7 @@
 }
 </style>
 
+
+<script setup lang="ts">
+const host = document.location.host;
+</script>
