b836b678a6f834cbc90ec779ce8b932f972d171d
vivax3794 <github@vivax3794.com>
2023-12-18 18:29:10 +0100

    add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+
+    vuetify: {
+        vuetifyOptions: {
+            theme: {
+                defaultTheme: "dark"
+            }
+        }
+    },
+
+    modules: ["vuetify-nuxt-module"]
 })
