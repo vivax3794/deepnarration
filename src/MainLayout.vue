78c911b5275ed851d7dd1cef6fd7ec03d631e7fc
Matisse <MatissesProjects@users.noreply.github.com>
2024-02-06 13:57:58 -0800

    adding combine image and image background removal, transparent and video removal

--- src/MainLayout.vue
+++ src/MainLayout.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar>
       <v-app-bar-nav-icon @click.stop="navdraw = !navdraw"></v-app-bar-nav-icon>
-      <v-app-bar-title>Deepnarration</v-app-bar-title>
+      <v-app-bar-title>JifForgeAI</v-app-bar-title>
 
       <router-link to="/user">
         <v-avatar id="avatar"
@@ -17,16 +17,26 @@
     <v-navigation-drawer expand-on-hover rail v-model="navdraw">
       <v-list nav>
         <v-list-item title="About" prepend-icon="mdi-information" to="/about"></v-list-item>
-        <v-list-item title="Create Image" prepend-icon="mdi-camera-enhance" to="/createImage"></v-list-item>
-        <v-list-item title="Retheme Image" prepend-icon="mdi-theme-light-dark" to="/rethemeImage"></v-list-item>
-        <v-list-item title="Similar Images" prepend-icon="mdi-image-multiple" to="/SimilarImages"></v-list-item>
-        <v-list-item title="Outpaint Image" prepend-icon="mdi-vector-rectangle" to="/OutpaintImage"></v-list-item>
+        <v-subheader>Image</v-subheader>
+        <v-list-item title="Create" prepend-icon="mdi-camera-enhance" to="/createImage"></v-list-item>
+        <v-list-item title="Similar" prepend-icon="mdi-image-multiple" to="/SimilarImages"></v-list-item>
+        <v-list-item title="Combine" prepend-icon="mdi-book-plus-multiple" to="/CombineImages"></v-list-item>
+        <v-list-item title="Retheme" prepend-icon="mdi-theme-light-dark" to="/rethemeImage"></v-list-item>
+        <v-list-item title="Outpaint" prepend-icon="mdi-vector-rectangle" to="/OutpaintImage"></v-list-item>
+        <v-list-item title="Background Removal" prepend-icon="mdi-image-minus-outline" to="/imageBackgroundRemoval"></v-list-item>
+
+        <v-subheader>GIF</v-subheader>
+        <v-list-item title="Dancer" prepend-icon="mdi-square-opacity" to="/transparentGif"></v-list-item>
+        <v-list-item title="Background Removal" prepend-icon="mdi-movie-remove-outline" to="/videoBackgroundRemoval"></v-list-item>
+
+        <v-subheader>Video</v-subheader>
         <v-list-item title="Face Swap" prepend-icon="mdi-face-recognition" to="/FaceSwap"></v-list-item>
-        <v-list-item title="Hologram" prepend-icon="mdi-hololens" to="/hologram"></v-list-item>
         <v-list-item title="Logo Emerge" prepend-icon="mdi-dropbox" to="/logoEmerge"></v-list-item>
-        <v-list-item title="Transparent Gif" prepend-icon="mdi-square-opacity" to="/transparentGif"></v-list-item>
         <v-list-item title="Parallax" prepend-icon="mdi-image-frame" to="/parallax"></v-list-item>
+
+        <!-- <v-list-item title="Hologram (looking glass only)" prepend-icon="mdi-hololens" to="/hologram"></v-list-item> -->
         <!-- <v-list-item title="Story (out of order)" prepend-icon="mdi-book" to="/story"></v-list-item> -->
+
       </v-list>
     </v-navigation-drawer>
     <v-main>
